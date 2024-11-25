34d6f3fa9b1e08b20b09efe6645c798573cee23f
Túlio Franco <tulioffranco199@gmail.com>
2024-11-25 12:48:08 -0300

    Feat: Exercicio 2.

--- minix/servers/pm/exec.c
+++ minix/servers/pm/exec.c
@@ -49,6 +49,9 @@ do_exec(void)
 	m.VFS_PM_FRAME_LEN = m_in.m_lc_pm_exec.framelen;
 	m.VFS_PM_PS_STR = m_in.m_lc_pm_exec.ps_str;
 
+	/* Adiciona o printf para exibir o comando executado */
+	printf("Executando: %s\n", (char *)m_in.m_lc_pm_exec.name);
+
 	tell_vfs(mp, &m);
 
 	/* Do not reply */
